a6e47a29884afbcb2f35ac0898fcdb45e86a278e
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2019-06-13 13:20:35 -0600

    add create to do button; add function for on click event; add basic styling

--- client/to-do/src/components/ToDoList/ToDoList.js
+++ client/to-do/src/components/ToDoList/ToDoList.js
@@ -17,17 +17,25 @@ export default class ToDoList extends Component {
       .then(data => this.setState({ items: data }));
   }
 
+  handleCreateToDo(event) {
+    event.preventDefault();
+  }
+
   render() {
     const { items } = this.state;
     return (
       <>
-        <ul className='list-group'>
-          {items.map(item =>
-            <li className='list-group-item' key={item.id}>
-              {item.description}
-            </li>
-          )}
-        </ul>
+        <h2 className='todo-header'>To Dos This Week</h2>
+        <div className='center-list'>
+          <ul className='list-group'>
+            {items.map(item =>
+              <li className='list-group-item' key={item.id}>
+                {item.description}
+              </li>
+            )}
+          </ul>
+          <button type="button" className="btn btn-success" onClick={this.handleCreateToDo}>Create To Do</button>
+        </div>
       </>
     );
   }
