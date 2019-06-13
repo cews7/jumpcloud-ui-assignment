09a496a6cb0ece60dc9f420ccd75d47210890c85
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2019-06-13 15:53:04 -0600

    update styling for list; add put request for updating description; add pencil favicon

--- client/to-do/src/components/ToDoList/ToDoList.js
+++ client/to-do/src/components/ToDoList/ToDoList.js
@@ -7,7 +7,8 @@ export default class ToDoList extends Component {
   constructor() {
     super();
     this.state = {
-      items: []
+      items: [],
+      currentItem: {}
     };
   }
 
@@ -21,20 +22,50 @@ export default class ToDoList extends Component {
     event.preventDefault();
   }
 
+  handleChange = async(item, event) => {
+    item.description = event.target.value
+
+    await this.setState({
+      currentItem: item
+    });
+
+    this.state.items.map(item => {
+      if (item.id === this.state.currentItem.id) {
+        item.description = this.state.currentItem.description
+      }
+    });
+  }
+
+  handleBlur = (item) => {
+    fetch(API + '/' + item.id, {
+      method: 'PUT',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response;
+    }).catch(error => error);
+  }
+
   render() {
     const { items } = this.state;
     return (
       <>
         <h2 className='todo-header'>To Dos This Week</h2>
         <div className='center-list'>
-          <ul className='list-group'>
+          <form className='form'>
             {items.map(item =>
-              <li className='list-group-item' key={item.id}>
-                {item.description}
-              </li>
+              <div className='form-group mb-2' key={item.id}>
+                <input type='text' className='form-control-plaintext center-list' value={item.description}
+                onChange={this.handleChange.bind(this, item)} onBlur={this.handleBlur.bind(this, item)} />
+                <button type="submit" className="btn btn-primary mb-2" onSubmit={this.handleIsDone}>
+                  { item.done ? 'Done' : 'Not Done' }
+                </button>
+              </div>
             )}
-          </ul>
-          <button type="button" className="btn btn-success" onClick={this.handleCreateToDo}>Create To Do</button>
+          </form>
+          <button type="submit" className="btn btn-success" onSubmit={this.handleCreateToDo}>Create To Do</button>
         </div>
       </>
     );
